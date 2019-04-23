bd9272903ef5b82fbaba6693bda2d9c08fc4d90c
malustewart <mastewart@itba.edu.ar>
2019-04-22 21:42:58 -0300

    histogram de la pdf exponencial tienen la media y varianza estimadas vs teoricas, agregue histograma de las medias y varianzas estimadas (punto 1d)

---
+++ ej1/plot_mu_var_histogram.m
@@ -0,0 +1,25 @@
+function [ mu, var ] = plot_mu_var_histogram( lambda, N_samples, reps )
+%PLOT_MU_VAR_HISTOGRAM Summary of this function goes here
+%   Detailed explanation goes here
+    hold off;
+    mu = ones(1,reps);
+    var = ones(1,reps);
+    
+    for i = 1:reps
+        exp_samples = montecarlo_exponencial(lambda, N_samples);
+        mu(i) = mean(exp_samples);
+        var(i) = std(exp_samples)^2;
+
+    end
+    
+    subplot(2,1,1);
+    histogram(mu);
+    title('Valor medio');
+    subplot(2,1,2);
+    histogram(var);
+    title('Varianza');
+    suptitle(['Valores estimados (lambda = ', num2str(lambda), ', Nro. de muestras = ', num2str(N_samples), ', repeticiones = ', num2str(reps)]);
+    
+
+end
+
